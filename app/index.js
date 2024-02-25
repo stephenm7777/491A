3e691acffab7444bd2344ab14eb25978e840af88
Stephen Martinez <122754065+stephenm7777@users.noreply.github.com>
2024-02-24 17:32:48 -0800

    Added logo images and text

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { View, Image, StyleSheet} from 'react-native';
 
 import { Link } from 'expo-router';
 
@@ -6,8 +6,7 @@ import { Link } from 'expo-router';
 export default function Page() {
   return (
     <View>
-      <Link href="/about">About</Link>
-      <Link href="/users/bacon">View user</Link>
+      <Image />
     </View>
   );
 }
